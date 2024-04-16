481e9913616ef04cfaefdbb46ff83a4c22bba1f9
“rachelzn” <racheltjengerari@gmail.com>
2024-04-16 23:50:56 +0700

    fix: import issue

--- src/service/product.rs
+++ src/service/product.rs
@@ -4,7 +4,7 @@ use rocket::serde::json::Json;
 use bambangshop::{Result, compose_error_response};
 use crate::model::product::Product;
 use crate::repository::product::ProductRepository;
-use super::service::notification::NotificationService;
+use crate::service::notification::NotificationService;
 
 pub struct ProductService;
 
@@ -15,7 +15,7 @@ impl ProductService {
 
         NotificationService.notify(&product_result.product_type, "CREATED",
             product_result.clone());
-            
+
         return Ok(product_result);
     }
 
