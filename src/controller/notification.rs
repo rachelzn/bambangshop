70b943f3707524bfbdf1c0f5d970d15241f6e601
“rachelzn” <racheltjengerari@gmail.com>
2024-04-16 18:12:54 +0700

    fix: Subscribe function in Notification controller and service

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -8,7 +8,7 @@ use crate::service::notification::NotificationService;
 #[post("/subscribe/<product_type>", data = "<subscriber>")]
 pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
     return match NotificationService::subscribe(product_type, subscriber.into_inner()) {
-        Ok(f) => Ok(Created::new("/")).body(Json::from(f)),
+       Ok(f) => Ok(Created::new("/").body(Json::from(f))),
         Err(e) => Err(e)
     };
 }
